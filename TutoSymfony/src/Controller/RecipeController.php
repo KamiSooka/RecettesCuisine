84a0386bcdc9d22d95449965348149cee5404573
Nico <delagrande.nico@gmail.com>
2024-04-21 01:46:51 +0200

    L'ORM Doctrine

--- TutoSymfony/src/Controller/RecipeController.php
+++ TutoSymfony/src/Controller/RecipeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,21 +13,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-       return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findWithDurationLowerThan(20);
+        return $this->render('recipe/index.html.twig', [
+           'recipes' => $recipes
+       ]);
+
     }
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', ['id' => $recipe->getId(), 'slug' => $recipe->getSlug()]);
+        }
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
-            'demo' => '<strong>Hello</strong>',
-            'person' => [
-                'firstanme' => 'John',
-                'lastname' => 'Doe',
-            ]
+            'recipe' => $recipe,
+
         ]);
     }
 }
